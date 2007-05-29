530f2c1e4ea6422cff1579b1de888a551cdff796
maierman <maierman@b38de4a9-2c31-0410-bb0d-d01ad7a04d2d>
2007-05-29 13:57:12 +0000

    git-svn-id: http://svn.tn123.ath.cx/trachack/TracDuplicates/0.10@14 b38de4a9-2c31-0410-bb0d-d01ad7a04d2d

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'TracDuplicates',
+    version = '0.1',
+    packages = ['tracduplicates'],
+    package_data = { 'tracduplicates': ['htdocs/*', 'templates/*' ] },
+
+    author = "Nils Maier",
+    author_email = "testnutzer123@gmail.com",
+    description = "Provides support for duplicates ticket references",
+    license = "BSD",
+    keywords = "trac plugin ticket dependencies duplicates",
+    url = "http://tn123.ath.cx/TracDuplicates",
+    classifiers = [
+        'Framework :: Trac',
+    ],
+    
+    entry_points = {
+        'trac.plugins': [
+            'tracduplicates.web_ui = tracduplicates.web_ui',
+        ]
+    }
+)
