3e066a063a945db5a74d8f461bb80d87adfffdd1
root <maierman@web.de>
2010-09-26 01:33:00 +0200

    Version 0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'TracDuplicates',
-    version = '0.1',
+    version = '0.11',
     packages = ['tracduplicates'],
     package_data = { 'tracduplicates': [ 'templates/*' ] },
 
